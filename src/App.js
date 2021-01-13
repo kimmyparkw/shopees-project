f44364621a39f98920b2127b8d532cc7e85800b6
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2021-01-12 21:23:29 -0600

    added main divs and basic styling

--- src/App.js
+++ src/App.js
@@ -3,9 +3,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
+        <h1>The Shoppies</h1>
+        <div className="searchbox">
+          <p>Movie title</p>
+          <input type="text" placeholder="Search..." />
+        </div>
+        <div className="list-containers">
+          <div className="results-container">
+            <h4>Results for "(add searched title here)"</h4>
+            <div className="results-lists">
+              <ul>
+                <li>search results listed here</li>
+                {/* <li> .map through all the search results</li> */}
+                {/* make sure to add nominate button to each result item */}
+              </ul>
+            </div>
+          </div>
+          <div className="nominations-container">
+            <h4>Nominations</h4>
+            <div className="nominations-list">
+              <ul>
+                <li>nominations go here</li>
+              </ul>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
